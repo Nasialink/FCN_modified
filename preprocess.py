Fix: normalization process minor fix

preprocess.py
@@ -102,13 +102,23 @@ for i in range(data.shape[1]):
 print(max_train,min_train)
 print(np.max(data[:,1,:,:,:]),np.max(data[:,2,:,:,:]),np.max(data[:,3,:,:,:]))
 
-min_train = np.array(min_train).reshape(4, 1, 1, 1)
-max_train = np.array(max_train).reshape(4, 1, 1, 1)
+# 185, 4, 160, 192, 128
+for w in range(3):
+    min_c = np.min(data[:, w, :, :])
+    max_c = np.max(data[:, w, :, :])
 
-for img in data:
-    img = (img - min_train) / (max_train - min_train)
+    data[:, w, :, :, :] = (data[:, w, :, :, :] - min_c) / (max_c - min_c)
 
-print(np.unique(labels))    
+
+
+# min_train = np.array(min_train).reshape(4, 1, 1, 1)
+# max_train = np.array(max_train).reshape(4, 1, 1, 1)
+
+# for img in data:
+#     img = (img - min_train) / (max_train - min_train)
+
+print(np.unique(labels)) 
+print("Min data: ", np.min(data), "Max data: ", np.max(data))   
 all_data= nib.Nifti1Image(data, affine=np.eye(4)) #converting np.arrays into nii.gz files
 all_lab= nib.Nifti1Image(labels, affine=np.eye(4))
 

train.py
@@ -37,9 +37,9 @@ save_pred_path = root_path+'/predictions'
 save_model_path = root_path+'/saved_model'
 test_file = root_path+'/testset'
 '''
-data_path = '/home/azach/FCN_modified_new/data/data_cropped.nii.gz'
-labels_path = '/home/azach/FCN_modified_new/data/labels_cropped.nii.gz'
-root_path='/home/azach/FCN_modified_new/data/training_'+ datetime.datetime.now().strftime("%m%d%Y_%H%M%S")
+data_path = '/home/azach/testdir/data/data_cropped.nii.gz'
+labels_path = '/home/azach/testdir/data/labels_cropped.nii.gz'
+root_path='/home/azach/testdir/data/training_'+ datetime.datetime.now().strftime("%m%d%Y_%H%M%S")
 #path_checkpoint = root_path +'/cp.ckpt'
 save_pred_path = root_path+'/predictions'
 save_model_path = root_path+'/saved_model'
@@ -93,6 +93,11 @@ print(np.max(np_data), np.min(np_data))
 # for i in data.shape[0]:
 #     data = sitk.GetArrayFromImage(data)
 # labels = sitk.GetArrayFromImage(labels)
+
+# data_train = np_data[:240, :, :, :, :]
+# labels_train = np_labels[:240, :, :, :]
+# data_test = np_data[240:, :, :, :, :]
+# labels_test = np_labels[240:, :, :, :]
 
 # Split into training and test set 
 data_train, data_test, labels_train, labels_test = train_test_split(np_data, np_labels, test_size=0.25, random_state=None)